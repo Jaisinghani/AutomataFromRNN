b6f08d9fdb0673a603fff8b0f0a2405fee3eebba
Amatthew512 <Amatthew1@knights.ucf.edu>
2018-11-24 18:59:35 -0500

    integrate words

--- FalseWordGenerator.py
+++ FalseWordGenerator.py
@@ -1,39 +1,42 @@
 import random
 
-class meaninglessWords():
+#class falseWordGenerator():
 
-    def __init__(alphabet, meaningfulWords):
-        self.wordList = []
+def generateFalseWords(alphabet, meaningfulWords):
+    wordList = []
+    #print (meaningfulWords)
+    #find the longest word
+    maxLength = 0
+    for word in meaningfulWords:
+        if len(word) > maxLength:
 
-        #find the longest word
-        maxLength = 0
-        for word in meaningfulWords:
-            if len(word) > maxLength:
-                maxLength = len(word)
-                
-        #init a length frequency array
-        lengthFrequenyArray = [range(maxLength)]
+            maxLength = len(word)
+
+    #init a length frequency array
+    lengthFrequenyArray = [0 for x in range(maxLength + 1)]
+
+    #calculate the frequencies
+    for word in meaningfulWords:
+        lengthFrequenyArray[len(word)] += 1
+        #print (lengthFrequenyArray)
+    
+        #create one meaningless word of length n for each meaningful word of length n
+   
+    #each word length
+    for length in range(len(lengthFrequenyArray)):
+   
+       #generate a number of words with that length
+       for times in range(lengthFrequenyArray[length]):
         
-        #calculate the frequencies
-        for word in meaningfulWords:
-            lengthFrequenyArray[len(word)] += 1
+          falseWord = ""
         
-       #create one meaningless word of length n for each meaningful word of length n
-       
-       #each word length
-       for length in range(len(lengthFrequenyArray)):
-       
-          #generate a number of words with that length
-          for times in range(lengthFrequenyArray[length])
-            
-            falseWord = ""
-            
-            #append one random letter to false word for each unit of length
-            for stringLength in range(length):
-                falseWord.append(random.choice.(alphabet))
-            if falseWord not in meaningfulWords:
-                self.wordList.append(falseWord)
-            
-    def getAllWords(self):
-        return self.wordList
+          #append one random letter to false word for each unit of length
+          for stringLength in range(length):
+              falseWord += random.choice(alphabet)
+          if falseWord.lower() not in meaningfulWords:
+              wordList.append(falseWord.lower())
+    #print (wordList)
+    return wordList
+    #def getAllWords(self):
+    #    return self.wordList
 
