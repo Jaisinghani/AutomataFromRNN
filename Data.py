481bd0dfb51ae0a757e388aa5231cb804a4f9948
Anirudha Nilegaonkar <anirudha.nilegaonkar@gmail.com>
2018-11-24 14:54:36 -0500

    Update Data.py

--- Data.py
+++ Data.py
@@ -1,24 +1,35 @@
 import sys
-
+import numpy as np
 #reading configuration file 
 with open('data.txt', 'r',newline='\n') as f:
     x = f.read().splitlines()
 
 x=list(x)
-Letters=['e','t','a','o','i','n','s','m','p','r','u']    
-meaning_words=[]
-for i in range(len(x)):
-	a=x[i]
-	flag=0
-	for j in a:
-		if(Letters.__contains__(j)):
-			continue	
+Letters_array=['e','t','a','o','i','n','s','r','u','m','p']    #
+Letters_array=np.array(Letters_array)
+
+length=[7,8,9,10,11]
+for l in length:
+	meaning_words=[]
+	name=str(l)+'.txt'
+	f= open(str(l)+'.txt',"w+")
+	print(l)
+	Letters=Letters_array[0:l]
+	print(Letters)
+	for i in range(len(x)):
+		a=x[i]
+		flag=0
+		for j in a:
+			if(Letters.__contains__(j)):
+				continue	
+			else:
+				flag=1
+				break
+		if(flag==0):
+			meaning_words.append(a)
+			#f.write(a)
 		else:
-			flag=1
-			break
-	if(flag==0):
-		meaning_words.append(a)
-	else:
-		continue	
-print(len(meaning_words))
-print(meaning_words)	
+			continue
+	f.write(str(meaning_words))
+	f.close() 
+	print(meaning_words)		
