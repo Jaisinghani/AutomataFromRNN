ef65d77502cad75a7223d0d2c327dca3eabc9f3c
amrutask <katkesamruta@gmail.com>
2018-11-26 14:51:53 -0500

    frequency analysis

---
+++ frequencyAnalysis.py
@@ -0,0 +1,70 @@
+def freqAnalysis():
+    frequencyAnalysis={}
+    letterList=[]
+    with open('words.txt', 'r') as f:
+        wordList=f.read()
+    tempDict=Counter()
+    for word in wordList:
+        tempDict[word]+=1     
+    notWord=["'",","," ","\n"]
+    frequencyAnalysis={}
+    for k in tempDict:
+        if k in notWord:
+            continue
+        else:
+            frequencyAnalysis.update({k:tempDict[k]})
+
+    resultFA=sorted(frequencyAnalysis.items(), key=lambda kv: kv[1],reverse=True)
+    frequencyAnalysis={}
+    for k in resultFA:
+        frequencyAnalysis.update({k[0]:k[1]})
+    #print(frequencyAnalysis)
+    for k in resultFA:
+        letterList.append(k[0]) 
+    return letterList
+
+def sentenceOrder(letterList): 
+    sentenceOrder=[]            #pass this value
+    letterMapping={}
+    with open('testSentences.txt', 'r') as f:    ## reading sentence from file
+        sentence=f.read()
+    tempDict=Counter()
+    for word in sentence:
+        tempDict[word]+=1       
+    notWord=[" ","\n"]
+    faSentence={}
+    for k in tempDict:
+        if k in notWord:
+            continue
+        else:
+            faSentence.update({k:tempDict[k]})
+    resultFaS=sorted(faSentence.items(), key=lambda kv: kv[1],reverse=True)
+    for k in resultFaS:
+        sentenceOrder.append(k[0]) 
+    #print(sentenceOrder)
+    for i,k in enumerate(sentenceOrder):
+        letterMapping.update({k[0]:letterList[i]})
+    return letterMapping,sentence
+
+def freqAnalysisSwap():
+    tempSentence=[]
+    sentenceString=""
+    tempWord=[]
+    letterList=freqAnalysis()
+    letterMapping,sentence=sentenceOrder(letterList)
+    for word in sentence.split():
+        tempString=" "
+        for l in word:
+            if l in letterMapping:
+                tempWord.append(letterMapping[l])
+            else:
+                continue
+
+        tempString.join(tempWord) 
+        tempWord.append(tempString)
+    result=sentenceString.join(tempWord)
+    return result
+
+from collections import Counter
+output=freqAnalysisSwap()
+print(output)
